595d90ab754bfd604c8634e683ff9a5ab0d6e8f3
Am0908 <as09082020@gmail.com>
2022-07-30 20:16:07 +0530

    emojipedia

---
+++ src/emojipedia.js
@@ -0,0 +1,44 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "🥺",
+      name: "Pleading Face Emoji",
+      meaning: 
+        "It is meant to represent the typical face one makes when pleading, that is, trying to win their compassion or sympathy. Besides conveying such acts as pleading, begging, or beseeching, the Pleading Face emoji also variously conveys sadness, guilt, cuteness, and even arousal."
+    },
+    {
+      emoji: "😍",
+      name: "Smiling Face With Heart-Shaped Eyes",
+      meaning: 
+        "This emoji is associated with adoration, love, or extreme positivity toward a specific subject (romantic or not)."
+    },
+    {
+      emoji: "😳",
+      name: "Flushed Face emoji",
+      meaning: 
+        "Let’s face it, Sky Ferreira was right, everything’s embarrassing: having a crush, tripping, farting, tampons falling out of your bag, tripping while farting and dropping tampons out of your bag in front of your crush … "
+    }
+  ];
+
+  export default emojipedia;
